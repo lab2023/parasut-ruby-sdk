3182b41ac58cdfd2eb93b57d722f19cc411b143b
seyyah <nurettin.senyer@gmail.com>
2019-03-20 23:03:53 +0300

    Example: SalesInvoice

--- examples/examples.rb
+++ examples/examples.rb
@@ -28,7 +28,7 @@ config['refresh_token'] = tokens.refresh_token
 account = ParasutRubySdk::REST::Account.new(config)
 puts account.all
 
-# v4 only
+# create
 p = {
   data: {
     id: "",
@@ -53,3 +53,73 @@ p = {
 }
 
 contact.create(p)
+
+# Product
+# -------------------------------------------------------------
+product = ParasutRubySdk::REST::Product.new(config)
+puts product.all
+
+# SalesInvoice
+# -------------------------------------------------------------
+sales_invoice = ParasutRubySdk::REST::SalesInvoice.new(config)
+puts sales_invoice.all
+puts sales_invoice.get(SALES_INVOICE_ID)
+
+# create
+p = {
+  data: {
+    type: "sales_invoices",
+    attributes: {
+      item_type: "invoice",
+      issue_date: "2019-03-20",
+
+      description: "description",
+      tax_office: "tax_office",
+      tax_number: "tax_number",
+      city: "city",
+      district: "district",
+      billing_address: "billing_address",
+      billing_phone: "billing_phone",
+      billing_fax: "billing_fax",
+      due_date: "2019-03-20",
+      invoice_id: 3,
+      invoice_series: "0001",
+    },
+    relationships: {
+      details: {
+        data: [
+          {
+            type: "sales_invoice_details",
+            attributes: {
+              quantity: 1,
+              unit_price: 99,
+              vat_rate: 0,
+              discount_type: "percentage",
+              discount_value: 0,
+              excise_duty_type: "percentage",
+              excise_duty_value: 0,
+              communications_tax_rate: 0,
+              #description: "satir description"
+            },
+            relationships: {
+              product: {
+                data: {
+                  id: "PRODUCT_ID",
+                  type: "products"
+                }
+              }
+            }
+          }
+        ]
+      },
+      contact: {
+        data: {
+          id: "CONTACT_ID",
+          type: "contacts"
+        }
+      },
+    }
+  }
+}
+
+sales_invoice.create(p)
