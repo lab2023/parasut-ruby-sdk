056a0d8606e65b5ae5cf0566b685374938d5d0e2
sabricakirr <sabri.cakir@lab2023.com>
2023-04-28 10:13:48 +0300

    Add class methods

---
+++ lib/parasut_ruby_sdk/rest/e_archive.rb
@@ -0,0 +1,22 @@
+module ParasutRubySdk
+  module REST
+    class Product < BaseRequest
+      def initialize(*args)
+        super(*args)
+        @resource = 'e_archives'
+      end
+
+      def get(id)
+        prepare_request(Util::RequestType::GET, "#{@resource}/#{id}")
+      end
+
+      def create(params)
+        prepare_request(Util::RequestType::POST, @resource, params)
+      end
+
+      def show_pdf(id)
+        prepare_request(Util::RequestType::GET, "#{@resource}/#{id}/pdf")
+      end
+    end
+  end
+end
